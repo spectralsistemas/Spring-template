d765ff16d4ff72d873a14be3a628e6d640e7e85c
Leo Guedes <leoguedesdev@gmail.com>
2025-09-10 22:32:21 -0300

    First Commit

---
+++ src/test/java/spectralsistemas/springtemplate/SpringTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package spectralsistemas.springtemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTemplateApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
